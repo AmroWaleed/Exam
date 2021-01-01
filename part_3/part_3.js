ec57276f49417982dbb84e145b22136caef0a13a
AmroWaleed <amro.alkharoof@gmail.com>
2021-01-01 22:45:46 +0200

    solve part 3

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -14,7 +14,7 @@ Q3) What are the main three sections of a JWT?
 
 
 Q4) Name three types of express middle-wares.
-
+router middle ware, handling error middleware and third party middle ware  
 
 Q5) What is the `effect` hook used for in react?
 
@@ -23,14 +23,15 @@ Q6) What is the `state` hook used for in react?
 
 
 Q7) List three thing you can use to create a responsive web application.
-
+create backend code side and frotend side like use SERN stack  
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
 
 Q9) What is the difference between authentication and authorization?
-
+authorization used for get access for web users who was registerd on website
+authintication used for check users if info of user that smae in the database   
 
 Q10) Why do we save some information as an environment variable (in .env file)?
-
+because we don`t need to share some information like data in database and node mudles 
 */
